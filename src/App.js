9bfe7816b08b1c01f0a63e380b66cab2c1062c35
Kranthi Kumar <kranthi.coder@gmail.com>
2017-02-15 20:58:12 -0600

    components in react

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component{
-  render(){
-    return(
-        <Buttons>
-          <button value="A">A</button>
-          <button value="B">B</button>
-          <button value="C">C</button>
-        </Buttons>
-      )
-  }
-}
-
-class Buttons extends React.Component{
-  constructor(){
-    super();
-    this.state= { selected: 'none' }
-  }
-
-    update(selected){
-      this.setState({selected})
-    }
-
-  render(){
-    let fn = child => 
-    React.cloneElement(child, {
-      onClick:this.update.bind(this, child.props.value)
-    })
-    let items = React.Children.map(this.props.children,fn);
-    return(
-          <div>
-        <h2>you have selected:{this.state.selected}</h2>
-        {items}
-      </div>
+class App extends Component {
+ 
+  render() {
+    
+    return (
+        <div>
+          <Header />
+          <Body />
+          <Button />
+          <Footer />           
+        </div>
     )
   }
-
 }
+
+const Header = () => <h1> This is header </h1>
+
+const Body = () => <p> This is body </p>
+
+const Footer = () => <span> This is footer </span>
+
+class Button extends Component{
+  render() {
+    return <button> Submit </button>
+  }
+}
+
 export default App;
